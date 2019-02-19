2d8072ec3b6d0ab817d1b92f16134fcd307c8de5
sergey <sergey@sk-developer.ru>
2019-02-19 15:56:30 +0300

    Update docker-compose Create additional scripts

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ db_host = env.get('DB_HOST', '127.0.0.1')
 db_password = env.get('DB_PASSWORD', '12345')
 
 SQLALCHEMY_DATABASE_URI = f'mysql://root:{db_password}@{db_host}/airtradex_protect'
-print(SQLALCHEMY_DATABASE_URI)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = '1234567890'
